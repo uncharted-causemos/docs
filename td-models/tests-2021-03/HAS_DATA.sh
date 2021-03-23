d5064d902554fb15f51d1b92aa9dcd487f4294b1
Daniel Chang <mwdchang@gmail.com>
2021-03-23 09:37:13 -0400

    add model-status, sleep

--- td-models/tests-2021-03/HAS_DATA.sh
+++ td-models/tests-2021-03/HAS_DATA.sh
@@ -3,13 +3,25 @@
 source ./vars.sh
 
 MODEL="HAS_DATA"
+SLEEP=5
 
 echo "Creating model with indicator data"
 time ./create-model.sh $HOST $AUTH "data/${MODEL}.json"
 
+echo "Sleeping $SLEEP"
+./model-status.sh $HOST $AUTH $MODEL
+sleep $SLEEP
+
+echo "Sleeping $SLEEP"
+./model-status.sh $HOST $AUTH $MODEL
+sleep $SLEEP
+
 echo "Running experiment"
 EID=$(./project.sh $HOST $AUTH $MODEL "data/project.json" | python -c \
     'import json,sys;print json.load(sys.stdin)["experimentId"]')
+
+echo "Sleeping 10s"
+sleep 10
 
 echo "Fetching experiment result $HOST $MODEL $EID"
 ./experiment-result.sh $HOST $AUTH $MODEL $EID
--- td-models/tests-2021-03/NO_DATA.sh
+++ td-models/tests-2021-03/NO_DATA.sh
@@ -3,13 +3,25 @@
 source ./vars.sh
 
 MODEL="NO_DATA"
+SLEEP=5
 
 echo "Creating model without indicator data"
 time ./create-model.sh $HOST $AUTH "data/${MODEL}.json"
 
+echo "Sleeping $SLEEP"
+./model-status.sh $HOST $AUTH $MODEL
+sleep $SLEEP
+
+echo "Sleeping $SLEEP"
+./model-status.sh $HOST $AUTH $MODEL
+sleep $SLEEP
+
 echo "Running experiment"
 EID=$(./project.sh $HOST $AUTH $MODEL "data/project.json" | python -c \
     'import json,sys;print json.load(sys.stdin)["experimentId"]')
+
+echo "Sleeping 10s"
+sleep 10
 
 echo "Fetching experiment result $EID"
 ./experiment-result.sh $HOST $AUTH $MODEL $EID
